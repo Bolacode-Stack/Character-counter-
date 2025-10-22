031d6451dca7012b488032fd8a2d1bca0cc4273d
Mobolaji Ajibola Jr <bolaex@yahoo.com>
2025-10-22 14:33:49 +0100

    Commit with character

--- Code/JS/counter.js
+++ Code/JS/counter.js
@@ -2,15 +2,15 @@ let output;
 const limit = document.querySelector(".limit-count");
 const spaces = document.querySelector(".spaces");
 const sentence = document.querySelector(".sentence-count");
-const setLimit = document.querySelector("#set-limit")
+const setLimit = document.querySelector("#set-limit");
 const characterInput = document.querySelector(".character-input");
 const totalCharacters = document.querySelector("#total-characters");
-const wordCount = document.querySelector(".word-count"); 
+const wordCount = document.querySelector(".word-count");
 const readingTime = document.querySelector(".reading-time");
 const container = document.querySelector(".contents");
 const wrapper = document.querySelector(".progress-wrapper");
-const toggle =  document.querySelector(".more-less");
-const icon = document.querySelector(".fa-solid")
+const toggle = document.querySelector(".more-less");
+const icon = document.querySelector(".fa-solid");
 const bar = document.querySelectorAll(".bar");
 const themeSwitcher = document.querySelector(".theme-switcher");
 const body = document.querySelector("body");
@@ -18,129 +18,128 @@ const logo = document.querySelector("#logo");
 const theme = document.querySelector(".theme");
 
 let boolean = true;
-let zero = 0, time = 50, second = 1000;
+let zero = 0,
+  time = 50,
+  second = 1000;
 let string = ["Analyze your text in real-time"];
 let regex = /\w+/g;
 
-setLimit.addEventListener("change", (event)  =>  {
-    if (setLimit.checked)   {
-        console.log(parseInt(limit.value))
-    }
+setLimit.addEventListener("change", (event) => {
+  if (setLimit.checked) {
+    console.log(parseInt(limit.value));
+  }
 });
 
-theme.addEventListener("click", (event)  =>  {    
-    body.classList.toggle("light-theme");
-    if (body.classList.contains('light-theme'))  {
-        logo.src ="/Assets/images/logo-light-theme.svg"; 
-        theme.src = "/Assets/images/icon-moon.svg";
-    } else {
-        logo.src ="/Assets/images/logo-dark-theme.svg"; 
-        theme.src = "/Assets/images/icon-sun.svg";
-    }
+theme.addEventListener("click", (event) => {
+  body.classList.toggle("light-theme");
+  if (body.classList.contains("light-theme")) {
+    logo.src = "/Assets/images/logo-light-theme.svg";
+    theme.src = "/Assets/images/icon-moon.svg";
+  } else {
+    logo.src = "/Assets/images/logo-dark-theme.svg";
+    theme.src = "/Assets/images/icon-sun.svg";
+  }
 });
 
-export function getContents()  {
-    let contents = [];
-    contents.push(characterInput.value);
-    return contents;
-};
-
-function countWords()  {
-    let match, count = zero;
-    let contents = getContents();
-    for (let content of contents)  {
-        if (match = content.match(/\w+/g))  {
-            count += match.length;
-            let timerID, word = zero
-            timerID = setInterval(()  =>  {
-                wordCount.innerText =  word += 1;
-                if (word == count)  {
-                    clearInterval(timerID)
-                }
-            }, time)
-        }
-     }
+export function getCharacters() {
+  let contents = [];
+  contents.push(characterInput.value);
+  return contents;
 }
 
-function countChar(count)  {
-    let totalCount = zero;
-    let contents = getContents();
-    for (let content of contents)  {
-        let splitText = content.split("");
-        count = splitText.length;
-        let timerID;
-        timerID = setInterval(()  =>  {
-            totalCharacters.innerText = totalCount += 1;
-            if (totalCount == splitText.length)  {
-                clearInterval(timerID);
-            } else if (splitText.length == zero)  {
-                totalCharacters.innerText = "00";
-            }
-        }, time)
+function countWords() {
+  let match,
+    count = zero;
+  let contents = getCharacters();
+  for (let content of contents) {
+    if ((match = content.match(/\w+/g))) {
+      count += match.length;
+      let timerID,
+        word = zero;
+      timerID = setInterval(() => {
+        wordCount.innerText = word += 1;
+        if (word == count) {
+          clearInterval(timerID);
+        }
+      }, time);
     }
-    return count;
+  }
 }
 
-function sentenceCount()  {
-    let match, count = zero;
-    let contents = getContents();
-    for (let content of contents)  {
-        if (match = content.match(/\.+/))  {
-            count += match.length;
-            console.log(count, match)
-            let timerID, word = 1;
-            timerID = setInterval(()  =>  {
-                sentence.innerText =  word += 1;
-                if (word == count)  {
-                    clearInterval(timerID)
-                }
-            }, time)
-        }
-     }
-};
+function countChar(count) {
+  let totalCount = zero;
+  let contents = getCharacters();
+  for (let content of contents) {
+    let splitText = content.split("");
+    count = splitText.length;
+    let timerID;
+    timerID = setInterval(() => {
+      totalCharacters.innerText = totalCount += 1;
+      if (totalCount == splitText.length) {
+        clearInterval(timerID);
+      } else if (splitText.length == zero) {
+        totalCharacters.innerText = "00";
+      }
+    }, time);
+  }
+  return count;
+}
 
-let timer, countdown = 60;
-timer = setInterval(()  =>  {
-    // readingTime.innerText = `${countdown--} seconds`;
-    if (countdown == zero)   {
-        readingTime.innerText = "0 seconds"
-        // characterInput.value = "";
-        clearInterval(timer);
+function sentenceCount(char) {
+  let count = 0;
+  let fullStop = letterCount(char);
+  let timerID = setInterval(() => {
+    sentence.innerText = count += 1;
+    if (count == fullStop) {
+      clearInterval(timerID);
     }
-}, time);  
+  });
+  return fullStop;
+}
 
-export function letterCount(char)  {
-    let counted = zero;
-    let contents = getContents();
-    for (let string of contents)  {
-        for (let i = 0; i < string.length; i++)  {
-            if (string[i] == char.toLowerCase())  {
-                    counted += 1;
-                }
-            }
-        }
-    return counted;
-}  
+let timer,
+  countdown = 60;
+timer = setInterval(() => {
+  // readingTime.innerText = `${countdown--} seconds`;
+  if (countdown == zero) {
+    readingTime.innerText = "0 seconds";
+    // characterInput.value = "";
+    clearInterval(timer);
+  }
+}, time);
 
-output = letterCount("r");
-// console.log(output);
+export function letterCount(char) {
+  let counted = zero;
+  let contents = getCharacters();
+  for (let string of contents) {
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] == char.toLowerCase()) {
+        counted += 1;
+      }
+    }
+  }
+  return counted;
+}
+
+output = letterCount(".");
+console.log(output);
 
 // Toggle letter density graph
-toggle.addEventListener("click", ()  =>  {
-        if (parseInt(wrapper.style.height) != wrapper.scrollHeight) {
-            wrapper.style.height = wrapper.scrollHeight + "px";
-            icon.classList.remove("fa-chevron-down")
-            icon.classList.add("fa-chevron-up");
-            console.log(wrapper.style.height);
-        } else {
-            icon.classList.remove("fa-chevron-up");
-            icon.classList.add("fa-chevron-down");
-            wrapper.style.height = "200px";
-        }
+toggle.addEventListener("click", () => {
+  if (parseInt(wrapper.style.height) != wrapper.scrollHeight) {
+    wrapper.style.height = wrapper.scrollHeight + "px";
+    icon.classList.remove("fa-chevron-down");
+    icon.classList.add("fa-chevron-up");
+    console.log(wrapper.style.height);
+  } else {
+    icon.classList.remove("fa-chevron-up");
+    icon.classList.add("fa-chevron-down");
+    wrapper.style.height = "200px";
+  }
 });
 
-window.onload = ()  =>  {
-        countWords();
-        countChar();
-        // sentenceCount();
-    };
+window.onload = () => {
+  countChar();
+  countWords();
+  sentenceCount(".");
+};
